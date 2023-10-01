9b0b295555d17e8dafaa1863c680fdc7b43edc4d
Felipe Cerezo <felipe.cerezo@hpe.com>
2023-10-01 13:50:10 +0200

    modified:   generate-files.sh modified:   start-stream-into-kafka.sh

--- generate-files.sh
+++ generate-files.sh
@@ -1,15 +1,21 @@
+
+
+
 NUM_FILES=$1
 SECONDS_BETWEEN_FILES=$2
 
 if [ "$NUM_FILES" == "" ]
 then
-   NUM_FILES=2
+   NUM_FILES=8
 fi
 
 if [ "$SECONDS_BETWEEN_FILES" == "" ]
 then
    SECONDS_BETWEEN_FILES=5
 fi
+
+echo Generate $NUM_FILES every $SECONDS_BETWEEN_FILES seconds
+echo
 
 docker exec -it flume java -cp /tmp/file-generator-1.0.0-dep.jar edu.doc_ti.jfcp.selec_reproc.gendata.FileGenerator -p /tmp/flume-input -s $SECONDS_BETWEEN_FILES -m $NUM_FILES
 
